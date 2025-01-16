4fbcf9f0e9ec866aa7abf31d0f9de58d02ebc9c7
Egor <egorunicorn@gmail.com>
2025-01-16 22:11:12 +0300

    Update test_pokemon.py

--- PythonProjects/test/test_pokemon.py
+++ PythonProjects/test/test_pokemon.py
@@ -2,7 +2,7 @@ import requests
 import pytest
 
 URL = 'https://api.pokemonbattle.ru/v2'
-TOKEN = '2f72e9a816b51eae65475c9faa253efd'
+TOKEN = 'dsafsjajfjr342jr324j'
 HEADER = {'Content-Type' : 'application/json', 'trainer_token' : TOKEN}
 TRAINER_ID = '18518'
 
